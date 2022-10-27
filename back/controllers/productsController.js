52a29621f089af0a93d3c93e217d911712f3d5ac
Paula Ramirez <parc56725@gmail.com>
2022-10-27 13:20:34 -0500

    C del CRUD

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,6 +1,20 @@
+const producto=require("../models/productos")
+
+//Ver la lista de productos
 exports.getProducts=(req,res,next) =>{
     res.status(200).json({
         success:true,
         message:"En esta ruta usted vera todos los productos"
     })
+}
+
+//Crear nuevo producto/api/productos
+exports.newProduct=async(req,res,next)=>{
+    const product= await producto.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+
 }
---
+++ back/models/productos.js
@@ -0,0 +1,88 @@
+const mongoose=require("mongoose")
+
+const productosSchema=mongoose.Schema({
+    nombre:{
+        type:String,
+        required:[true,"Por favor registra el nombre del producto."],
+        trim:true,
+        maxLength:[120,"El nombre del producto no debe exceder los 120 caractares."]
+    },
+    precio:{
+        type: Number,
+        required:[true,"Por favor registre el precio del producto."],
+        maxLength:[8,"El precio del producto no puede estar por encima de 99'999.999"],
+        default: 0.0
+    },
+    descripcion:{
+        type:String,
+        required:[true,"Por favor registre una descrpcion para el producto."]
+    },
+    calificacion:{
+        type: Number,
+        default: 0
+    },
+    imagen:[
+        {
+            public_id:{
+                type:String,
+                required:true
+            },
+            url:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    categoria:{
+        type:String,
+        required:[true,"Por favor seleccione la categoria del producto."],
+        enum:{
+            values:[
+                "Cargador de pared para celulares",
+                "Cargador inalambrico para celulares",
+                "Forros resistentes para celulares",
+                "Forros de plactico para celulares",
+                "Cable USB para telefonos moviles",
+                "PopSocKets",
+                "Vidrios templados para celulares",
+            ]
+        }
+    },
+    vendedor:{
+        type:String,
+        required:[true,"Pro favor registre el vdendor de producto"]
+    },
+    inventario:{
+        type: Number,
+        required:[true,"Por favor registre el stock del producto"],
+        maxLength:[5,"Cantidad maxima del producto no puede sobrepasar 99999"],
+        default: 0
+    },
+    numCalificaciones:{
+        type: Number,
+        default: 0
+    },
+    opiniones:[
+        {
+            nombreCliente:{
+                type:String,
+                required:true
+            },
+            rating:{
+                type: Number,
+                required:true
+            },
+            comentario:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    fechaCreacion:{
+        type:Date,
+        default:Date.now
+    }
+
+})
+
+module.exports=mongoose.model("productos",productosSchema)
