9c9c475b951b4dc06005c3b1d99108187c10d3be
Paula Ramirez <parc56725@gmail.com>
2022-11-01 15:30:54 -0500

    logramos tener una pagina que responde desde el localhost

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Tienda de accesorios para celulares Cel-Kit</h1>
+        <img src="./images/Cel_kit.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
