09ff6eedf7c4b15c582ff712d13882afaafe5302
Paula Ramirez <parc56725@gmail.com>
2022-11-08 10:56:07 -0500

    configuracion y creacion de carpetas para el funcionamiento del back y front de la app

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
 import Home from './components/Home';
+import { ProductDetails } from './components/products/ProductDetails';
 //Router traido desde react-router-dom (no confundir con el express)
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -10,16 +11,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <Router>
-    <div className="App">
+      <div className="App">
         <Header />
         <div className='container container-fluid'>
           <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/Home" element={<Home />}/>
-          </Routes>  
+            <Route path="/" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/producto/:id" element={<ProductDetails />} />
+          </Routes>
         </div>
         <Footer />
-    </div>
+      </div>
     </Router>
   );
 }
--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,194 +1,58 @@
 import React, { Fragment, useEffect } from 'react'
 import MetaData from './layout/MetaData'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { getProducts } from '../actions/productActions'
+import { Link } from 'react-router-dom'
+import { useAlert} from 'react-alert'
 
 export const Home = () => {
+    const { loading, productos, error} = useSelector(state=> state.products)
+    const alert= useAlert();
+
     const dispatch = useDispatch();
-    useEffect(()=>{
+    useEffect(() => {
+        if (error){
+            return alert.error(error)
+        }
+
         dispatch(getProducts());
     }, [dispatch])
 
 
     return (
         <Fragment>
-            <MetaData title="Decora tu móvil con nuestros productos"></MetaData>
+            {loading ? <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
+                <Fragment>
+                    <MetaData title="Decora tu móvil con nuestros productos"></MetaData>
             <h1 id="encabezado_productos">Ultimos Productos</h1>
 
             <section id="productos" className='container mt-5'>
                 <div className='row'>
-                    {/*Producto 1*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                    {productos && productos.map (producto => (
+                        <div key={producto._id} className='col-sm-12 col-md-6 col-lg-3 my-3'>
                         <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/auricular_inalambrico.png' alt="Auricular Inalambrico"></img>
+                            <img className='card-img-top mx-auto' src={producto.imagen[0].url} alt={producto.imagen[0].public_id}></img>
                             <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Auricular Inalambrico Para Celular</a></h5>
+                                <h5 id="titulo_producto"><Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
                                 <div className='rating mt-auto'>
                                     <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
+                                        <div className='rating-inner' style={{width: `${(producto.calificacion/5)*100}%`}}></div>
                                     </div>
-                                    <span id="No_de_Opiniones"> 20 reviews</span>
+                                    <span id="No_de_Opiniones"> {producto.numCalificaciones} Reviews</span>
                                 </div>
-                                <p className='card-text'>$80.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                <p className='card-text'>{producto.precio}</p><Link to={`/producto/${producto._id}`} id="view_btn" className='btn btn-block'>
                                     Ver detalle
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
-
-                    {/*Producto 2*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/cargador_inalambrico.jpg' alt="Cargador  Inalambrico"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Cargador Inalambrico Para Celular</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_Opiniones"> 20 reviews</span>
-                                </div>
-                                <p className='card-text'>$100.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
+                    
+                    ))}
                     </div>
-
-                    {/*Producto 3*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/cargador.jpg' alt="Cargador de Cable "></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Cargador de Cable Para Celular</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_Opiniones"> 20 reviews</span>
-                                </div>
-                                <p className='card-text'>$60.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 4*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/colga.jpg' alt="starphone colgantes"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Starphone Colgantes Para Celular</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_Opiniones"> 10 reviews</span>
-                                </div>
-                                <p className='card-text'>$20.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 5*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/cubre_cable.jpg' alt="Cubre Cable"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Cubre Cable Para Cargador</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_Opiniones"> 10 reviews</span>
-                                </div>
-                                <p className='card-text'>$10.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 6*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/diadema.jpg' alt="Diadema"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Diadema Para Celular</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_Opiniones"> 20 reviews</span>
-                                </div>
-                                <p className='card-text'>$90.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 7*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/forros.jpg' alt="Forros Personalizados"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Forros Personalizado Para Celular</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_Opiniones"> 15 reviews</span>
-                                </div>
-                                <p className='card-text'>$30.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 8*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/pop_so.jpg' alt="Pop Socket"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Pop Socket Para Celular</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_Opiniones"> 10 reviews</span>
-                                </div>
-                                <p className='card-text'>$15.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 9*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/sopo.jpg' alt="Soporte"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Soporte Para Celular</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_Opiniones"> 15 reviews</span>
-                                </div>
-                                <p className='card-text'>$50.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </section>
+                </Fragment>
+            )}
+            
 
         </Fragment>
     )
