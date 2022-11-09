5db2f76ed9e8ffc14ece6419436f258b719e96ec
Paula Ramirez <parc56725@gmail.com>
2022-11-08 20:38:24 -0500

    modelo usuario

--- back/app.js
+++ back/app.js
@@ -5,9 +5,11 @@ const errorMiddleware= require("./middleware/errors")
 app.use(express.json());
 
 //Importar rutas
-const productos=require("./routers/products")
+const productos=require("./routes/products")
+const usuarios=require("./routes/auth")
 
 app.use('/api',productos) //Sujeto a decision (ruta del navegador)
+app.use('/api',usuarios)
 
 //MiddleWares para manejar errores
 app.use(errorMiddleware)
--- back/middleware/errors.js
+++ back/middleware/errors.js
@@ -6,7 +6,7 @@ module.exports = (err, req, res, next) =>{
     
     res.status(err.statusCode).json({
         success:false,
-        message: err
+        message: err.stack
     })
-    
+
 }
